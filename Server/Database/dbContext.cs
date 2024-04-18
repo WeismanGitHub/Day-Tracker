d864f7d199223ff9aee2898fa74440b9fba88cd3
weisman <weisman123@outlook.com>
2024-04-17 21:42:09 -0700

    Create dbContext

---
+++ Server/Database/dbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace Server.Database;
+
+using Microsoft.EntityFrameworkCore;
+using Server.Database.Models;
+using System;
+
+
+public class DayTrackerContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public string DbPath { get; }
+
+    public DayTrackerContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "daytracker.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+        options.UseSqlite($"Data Source={DbPath}");
+}
