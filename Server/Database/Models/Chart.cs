474e81983e86a1bbbdd35999c0441d200ce87dc0
weisman <weisman123@outlook.com>
2024-04-17 22:07:32 -0700

    Create Chart table

---
+++ Server/Database/Models/Chart.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Database.Models;
+
+public enum ChartType {
+	Counter,
+	CheckMark,
+	Scale
+}
+
+public class Chart {
+	[Key, Required]
+	public required Guid Id { get; set; } = Guid.NewGuid();
+	[Required]
+	public required Guid UserId { get; set; }
+	[Required]
+	public required string Name { get; set; }
+	[Required]
+	public required ChartType Type { get; set; }
+	[Required]
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
