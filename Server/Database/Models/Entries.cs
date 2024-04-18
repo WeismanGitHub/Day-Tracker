188fdcdaf5f07d51223f34ff6bb01eaa4e5afbb0
weisman <weisman123@outlook.com>
2024-04-17 22:29:24 -0700

    Merge entry files/models

---
+++ Server/Database/Models/Entries.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Server.Database.Models;
+
+public class EntryBase
+{
+    [Key, Required]
+    public required Guid Id { get; set; } = Guid.NewGuid();
+
+    [Required]
+    public required Guid ChartId { get; set; }
+
+    [Required]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public required Chart Chart { get; set; }
+}
+
+public class CounterEntry : EntryBase
+{
+    [Required]
+    public required uint Counter { get; set; }
+}
+
+public class ScaleEntry : EntryBase
+{
+    [Required]
+    public required uint Rating { get; set; }
+}
+
+public class CheckmarkEntry : EntryBase
+{
+    [Required]
+    public required bool Checked { get; set; }
+}
