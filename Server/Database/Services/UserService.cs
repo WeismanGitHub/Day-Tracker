038af7f346ab5ffa71e8535c52623b23cc2daa15
weisman <weisman123@outlook.com>
2024-04-21 19:33:40 -0700

    Add method

--- Server/Database/Services/UserService.cs
+++ Server/Database/Services/UserService.cs
@@ -1,6 +1,13 @@
-﻿namespace Server.Database.Services;
+﻿using Server.Database.Models;
+
+namespace Server.Database.Services;
 
 public class UserService
 {
     public DayTrackerContext _context { get; set; } = new();
+
+    public async Task<User?> GetUser(Guid userId)
+    {
+        return await _context.Users.FindAsync(userId);
+    }
 }
