500fe37016bf9fc4737466f503869491422c5722
weisman <weisman123@outlook.com>
2024-04-23 21:00:21 -0700

    Use FetchMetadataMiddleware

--- Server/Services.cs
+++ Server/Services.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using System.Text;
+using Hellang.Middleware.ProblemDetails;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace Server;
 
 public static class Services
 {
-    public static void Add(WebApplicationBuilder builder)
+    public static void Add(WebApplicationBuilder builder, Configuration config)
     {
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-
         builder.Services.AddSwaggerGen(x =>
             x.SwaggerDoc(
                 "v1",
@@ -25,14 +25,20 @@ public static class Services
 
         builder
             .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(
-                JwtBearerDefaults.AuthenticationScheme,
-                options => builder.Configuration.Bind("JwtSettings", options)
-            )
-            .AddCookie(
-                CookieAuthenticationDefaults.AuthenticationScheme,
-                options => builder.Configuration.Bind("CookieSettings", options)
-            );
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = config.JWT.ValidIssuer,
+                    ValidAudience = config.JWT.ValidAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(config.JWT.Secret)
+                    )
+                };
+            });
     }
 
     public static void Use(WebApplication app)
@@ -43,7 +49,9 @@ public static class Services
             app.UseSwaggerUI();
         }
 
+        app.UseMiddleware<FetchMetadataMiddleware>();
         app.UseHttpsRedirection();
+        app.UseAuthentication();
         app.UseSecurityHeaders(SecurityHeadersPolicy.Create());
         app.UseHsts();
 
