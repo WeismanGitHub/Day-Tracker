874369cb7632e61a42f7f8fff97b8b704484f216
weisman <weisman123@outlook.com>
2024-04-23 20:50:27 -0700

    Create custom exception classes

---
+++ Server/Exceptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Server;
+
+public class CustomException : Exception
+{
+    public CustomException(string message)
+        : base(message) { }
+}
+
+public class UsernameTakenException : CustomException
+{
+    public UsernameTakenException(string message = "This username has been taken.")
+        : base(message) { }
+}
