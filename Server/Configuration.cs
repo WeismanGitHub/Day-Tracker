fc7148f13c047efc9e90a285e9e0ec1a83ea04a7
weisman <weisman123@outlook.com>
2024-04-24 21:14:35 -0700

    Change config structure

--- Server/Configuration.cs
+++ Server/Configuration.cs
@@ -1,3 +1,13 @@
 ﻿namespace Server;
 
-public class Configuration { }
+public class JwtConfig
+{
+    public required string Secret { get; set; }
+    public required string ValidIssuer { get; set; }
+    public required string ValidAudience { get; set; }
+}
+
+public class Configuration
+{
+    public required JwtConfig Jwt { get; set; }
+}
