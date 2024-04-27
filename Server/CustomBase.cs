2b84cc04a48217566294f701bedb380b84c0c19c
weisman <weisman123@outlook.com>
2024-04-27 01:43:17 -0700

    Add Authorize attribute

--- Server/CustomBase.cs
+++ Server/CustomBase.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Server;
 
 [ApiController]
+[Authorize]
 [Route("Api/[controller]")]
 public abstract class CustomBase : ControllerBase { }
