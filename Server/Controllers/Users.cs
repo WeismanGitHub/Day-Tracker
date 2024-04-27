ef2e9a6e47611efa0caf8222a46c6657dcc8eb0b
weisman <weisman123@outlook.com>
2024-04-27 02:57:34 -0700

    Add some fluent validation stuff

--- Server/Controllers/Users.cs
+++ Server/Controllers/Users.cs
@@ -1,22 +1,78 @@
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FluentValidation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Server.Database.Models;
+using Microsoft.IdentityModel.Tokens;
 using Server.Database.Services;
 
 namespace Server.Controllers;
 
 public class UsersController : CustomBase
 {
-    [HttpGet(Name = "/")]
-    public void Get(UserService service)
+    public class SignupModel
     {
-        service._context.Charts.Add(
-            new()
-            {
-                Name = "dasdas",
-                Type = ChartType.Counter,
-                UserId = new Guid("6E380295-271F-47E7-9588-0EBA51F4E18A")
-            }
+        [Required]
+        public required string Name { get; set; }
+
+        [Required]
+        public required string Password { get; set; }
+    }
+
+    private class Validator : AbstractValidator<SignupModel>
+    {
+        public Validator()
+        {
+            RuleFor(u => u.Name)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(50)
+                .WithMessage("Username must be between 1 and 50 characters.");
+
+            RuleFor(u => u.Password)
+                .NotEmpty()
+                .NotNull()
+                .MaximumLength(72)
+                .Must(p => p.IsValidPassword())
+                .WithMessage("Password is invalid.");
+        }
+    }
+
+    [ProducesResponseType(StatusCodes.Status201Created | StatusCodes.Status400BadRequest)]
+    [AllowAnonymous]
+    [Tags("Create", "User")]
+    [HttpPost(Name = "Signup")]
+    public async Task<IActionResult> Signup(
+        HttpResponse res,
+        [FromBody] SignupModel model,
+        UserService service,
+        Configuration config
+    )
+    {
+        var exists = await service.UserExists(model.Name);
+
+        if (exists)
+        {
+            throw new UsernameTakenException();
+        }
+
+        var user = await service.CreateUser(model.Name, model.Password);
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.Jwt.Secret));
+        var claims = new List<Claim>() { new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()) };
+
+        var token = new JwtSecurityToken(
+            issuer: config.Jwt.ValidIssuer,
+            audience: config.Jwt.ValidAudience,
+            expires: DateTime.Now.AddMonths(1),
+            claims: claims,
+            signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
         );
-        service._context.SaveChanges();
+
+        res.Cookies.Append("AuthCookie", token.ToString());
+
+        return Created();
     }
 }
