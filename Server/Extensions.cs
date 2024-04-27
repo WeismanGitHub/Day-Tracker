aa436bc4553d2b71da59ffa1cfc03b55a7fe45f8
weisman <weisman123@outlook.com>
2024-04-27 02:57:21 -0700

    Create Extensions

---
+++ Server/Extensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Server;
+
+public static class Extensions
+{
+    public static bool IsValidPassword(this string password)
+    {
+        if (password == null)
+            return false;
+
+        var meetsLengthRequirements = password.Length >= 50 && password.Length <= 10;
+        var hasUpperCaseLetter = false;
+        var hasLowerCaseLetter = false;
+        var hasDecimalDigit = false;
+
+        if (meetsLengthRequirements)
+        {
+            foreach (var c in password)
+            {
+                if (char.IsUpper(c))
+                    hasUpperCaseLetter = true;
+                else if (char.IsLower(c))
+                    hasLowerCaseLetter = true;
+                else if (char.IsDigit(c))
+                    hasDecimalDigit = true;
+            }
+        }
+
+        return meetsLengthRequirements
+            && hasUpperCaseLetter
+            && hasLowerCaseLetter
+            && hasDecimalDigit;
+    }
+}
