a487385cfc88297e8d6d8a85ced32d437741fe4d
weisman <weisman123@outlook.com>
2024-04-28 19:04:48 -0700

    Add auth to route

--- Client/src/router.tsx
+++ Client/src/router.tsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from './pages/home.tsx';
+import Auth from './pages/auth.tsx';
 
-const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+const router = createBrowserRouter([
+    { path: '/', element: <Home /> },
+    { path: '/auth', element: <Auth /> },
+]);
 
 export default router;
