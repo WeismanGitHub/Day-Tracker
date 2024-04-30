3bbcc7a16955b02205f80c577bb7df12acc0020c
weisman <weisman123@outlook.com>
2024-04-30 04:24:28 -0700

    Hash passwords

--- Server/Database/Services/UserService.cs
+++ Server/Database/Services/UserService.cs
@@ -19,7 +19,10 @@ public class UserService
 
     public async Task<User> CreateUser(string name, string password)
     {
-        var user = await _context.Users.AddAsync(new() { Name = name, Password = password });
+        var user = await _context.Users.AddAsync(
+            new() { Name = name, Password = BCrypt.Net.BCrypt.HashPassword(password) }
+        );
+
         await _context.SaveChangesAsync();
 
         return user.Entity;
