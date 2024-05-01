d4a8eeed5e6c5d59b8d5310bbd3fdcdb99f150e6
weisman <weisman123@outlook.com>
2024-05-01 02:18:30 -0700

    Simplify namespace for Bcrypt

--- Server/Database/Services/UserService.cs
+++ Server/Database/Services/UserService.cs
@@ -20,7 +20,7 @@ public class UserService
     public async Task<User> CreateUser(string name, string password)
     {
         var user = await _context.Users.AddAsync(
-            new() { Name = name, Password = BCrypt.Net.BCrypt.HashPassword(password) }
+            new() { Name = name, Password = HashPassword(password) }
         );
 
         await _context.SaveChangesAsync();
