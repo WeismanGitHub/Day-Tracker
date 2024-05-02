5177da6162e62adda5728216e172896570afedff
weisman <weisman123@outlook.com>
2024-05-02 01:53:50 -0700

    Remove unnecessary using statements

--- Server/Controllers/Users.cs
+++ Server/Controllers/Users.cs
@@ -1,7 +1,5 @@
 using System.Security.Claims;
 using FluentValidation;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Server.Database.Services;
