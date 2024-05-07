838d2f9bd1f5e6d587a5e06be81b30debce65f16
weisman <weisman123@outlook.com>
2024-05-07 14:48:08 -0700

    Create EntryService

--- Server/AppUtilities.cs
+++ Server/AppUtilities.cs
@@ -15,6 +15,7 @@ public static class AppUtilities
     {
         services.AddSingleton(config);
         services.AddScoped<UserService>();
+        services.AddScoped<EntryService>();
         services.AddScoped<ChartService>();
 
         services.AddControllers();
---
+++ Server/Database/Services/EntryService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Database.Models;
+
+namespace Server.Database.Services;
+
+public class EntryService
+{
+    public DayTrackerContext _context { get; set; } = new();
+
+    public async Task AddCounterEntry(Chart chart, CounterEntry entry)
+    {
+        var existingEntry = await _context
+            .Entries.Where(e => (e.ChartId == chart.Id) && e.CreatedAt.Date == entry.CreatedAt.Date)
+            .SingleOrDefaultAsync();
+
+        if (existingEntry is not null)
+        {
+            throw new BadRequestException("An entry for today already exists.");
+        }
+
+        _context.Entries.Add(entry);
+        await _context.SaveChangesAsync();
+    }
+}
