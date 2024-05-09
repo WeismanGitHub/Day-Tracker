ce41820459f5fa0342e08b9244e3bdb7cbb377ac
weisman <weisman123@outlook.com>
2024-05-09 10:47:33 -0700

    Finish CreateChart endpoint

--- Server/Controllers/Entries.cs
+++ Server/Controllers/Entries.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
 using Server.Database.Models;
 using Server.Database.Services;
 
@@ -10,8 +11,27 @@ public class EntriesController : CustomBase
 {
     public class CreateEntryBody
     {
+        public required ChartType Type { get; set; }
+        public uint? Rating { get; set; }
+        public bool? Checked { get; set; }
         public uint? Count { get; set; }
-        public DateTimeOffset Date { get; set; }
+        public required DateTimeOffset Date { get; set; }
+    }
+
+    private class CreateEntryValidator : AbstractValidator<CreateEntryBody>
+    {
+        public CreateEntryValidator()
+        {
+            RuleFor(e => e.Type)
+                .NotEmpty()
+                .NotNull()
+                .Must(t => Enum.IsDefined(typeof(ChartType), t))
+                .WithMessage("Invalid chart type.");
+
+            RuleFor(e => e)
+                .Must(e => e.Count is not null || e.Checked is not null || e.Rating is not null)
+                .WithMessage("Must have a value for the entry.");
+        }
     }
 
     public class EntryIdDTO
@@ -19,12 +39,12 @@ public class EntriesController : CustomBase
         public required Guid Id { get; set; }
     }
 
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(EntryIdDTO), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [HttpPost(Name = "CreateEntry")]
-    public async Task<IActionResult> CreateCounterEntry(
+    public async Task<IActionResult> CreateEntry(
         [FromRoute] Guid chartId,
         [FromBody] CreateEntryBody body,
         ChartService chartService,
@@ -39,20 +59,54 @@ public class EntriesController : CustomBase
             throw new NotFoundException("Could not find chart.");
         }
 
-        if (chart.Type is not ChartType.Counter)
+        var entry = CreateEntryObject(body, chart);
+        await entryService.CreateEntry(chart, entry);
+
+        return Ok(new EntryIdDTO() { Id = entry.Id });
+    }
+
+    private static Entry CreateEntryObject(CreateEntryBody body, Chart chart)
+    {
+        switch (chart.Type)
         {
-            throw new BadRequestException("Invalid chart type.");
+            case ChartType.Scale:
+                if (body.Rating is null)
+                {
+                    throw new BadRequestException("Rating cannot be null in a scale entry.");
+                }
+
+                return new ScaleEntry()
+                {
+                    ChartId = chart.Id,
+                    Rating = (uint)body.Rating,
+                    CreatedAt = body.Date
+                };
+            case ChartType.Counter:
+                if (body.Count is null)
+                {
+                    throw new BadRequestException("Count cannot be null in a counter entry.");
+                }
+
+                return new CounterEntry()
+                {
+                    ChartId = chart.Id,
+                    Count = (uint)body.Count,
+                    CreatedAt = body.Date
+                };
+            case ChartType.CheckMark:
+                if (body.Checked is null)
+                {
+                    throw new BadRequestException("Checked cannot be null in a checkmark entry.");
+                }
+
+                return new CheckmarkEntry()
+                {
+                    ChartId = chart.Id,
+                    Checked = (bool)body.Checked,
+                    CreatedAt = body.Date
+                };
+            default:
+                throw new BadRequestException("Invalid Type");
         }
-
-        var entry = new CounterEntry()
-        {
-            ChartId = chartId,
-            Count = body.Count,
-            CreatedAt = body.Date
-        };
-
-        await entryService.CreateCounterEntry(chart, entry);
-
-        return Ok();
     }
 }
