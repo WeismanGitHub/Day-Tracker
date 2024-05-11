4e7f392a1d913fc0dee8be6dfd5f41f6b5dc68da
weisman <weisman123@outlook.com>
2024-05-11 13:10:57 -0700

    Add Signin/Signout button

--- Client/src/navbar.tsx
+++ Client/src/navbar.tsx
@@ -1,10 +1,73 @@
+import { ToastContainer, Toast } from 'react-bootstrap';
+import { problemDetailsSchema } from './schemas';
 import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBar() {
+    const [authenticated, setAuthenticated] = useState(Boolean(localStorage.getItem('authenticated')));
+    const [error, setError] = useState<CustomError | null>(null);
+    const [success, setSuccess] = useState(false)
     const [isNavOpen, setIsNavOpen] = useState(false);
+    const navigate = useNavigate()
+
+    async function signout() {
+        if (!window.confirm('Are you sure you want to sign out?')) {
+            return;
+        }
+
+        try {
+            await axios.post('Api/Users/Account/SignOut');
+            localStorage.removeItem('authenticated');
+            setAuthenticated(false);
+            setSuccess(true);
+        } catch (err) {
+            if (
+                axios.isAxiosError<CustomError>(err) &&
+                problemDetailsSchema.isValidSync(err.response?.data)
+            ) {
+                setError(err.response.data);
+            } else {
+                setError({
+                    title: 'Unknown Error',
+                    detail: 'Something went wrong!',
+                    status: 500,
+                });
+            }
+        }
+    }
 
     return (
         <>
+            <ToastContainer position="top-end">
+                <Toast
+                    onClose={() => setError(null)}
+                    show={error !== null}
+                    autohide={true}
+                    className="d-inline-block m-1"
+                    bg={'danger'}
+                >
+                    <Toast.Header>
+                        <strong className="me-auto">{error?.title}</strong>
+                    </Toast.Header>
+                    <Toast.Body>{error?.detail}</Toast.Body>
+                </Toast>
+            </ToastContainer>
+            <ToastContainer position="top-end">
+                <Toast
+                    onClose={() => setSuccess(false)}
+                    show={success}
+                    autohide={true}
+                    className="d-inline-block m-1"
+                    bg={'success'}
+                >
+                    <Toast.Header>
+                        <strong className="me-auto">Success!</strong>
+                    </Toast.Header>
+                    <Toast.Body>Signed out</Toast.Body>
+                </Toast>
+            </ToastContainer>
+
             <nav
                 className="navbar navbar-expand-md py-1 ps-2 pe-2"
                 style={{ textAlign: 'center', fontSize: 'x-large', backgroundColor: '#9CC3FF' }}
@@ -50,6 +113,11 @@ export default function NavBar() {
                                     style={{ color: 'white' }}
                                 >
                                     GitHub
+                                </a>
+                            </li>
+                            <li className={`m-1 w-75 ${isNavOpen ? ' mb-2' : ''}`}>
+                                <a className="nav-item active w-100" onClick={() => authenticated ? signout() : navigate("/Auth")} style={{ color: 'white' }}>
+                                    {authenticated ? "Sign Out" : "Sign In"}
                                 </a>
                             </li>
                         </ul>
