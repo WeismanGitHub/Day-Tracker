dbe57288c8150e4f4ea8f2bab36642fc17e1503f
weisman <weisman123@outlook.com>
2024-05-13 17:17:16 -0700

    Add account page

---
+++ Client/src/pages/account.tsx
@@ -0,0 +1,10 @@
+import NavBar from '../navbar';
+
+export default function Account() {
+    return (
+        <>
+            <NavBar />
+            hello world
+        </>
+    );
+}
--- Client/src/router.tsx
+++ Client/src/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import Home from './pages/home.tsx';
 import Auth from './pages/auth.tsx';
+import Account from './pages/account.tsx';
 
 const router = createBrowserRouter([
     { path: '/', element: <Home /> },
     { path: '/auth', element: <Auth /> },
+    { path: '/account', element: <Account /> },
 ]);
 
 export default router;
