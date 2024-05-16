9a55f6f237a1ed79bc2412adefab1abafb72764f
weisman <weisman123@outlook.com>
2024-05-15 22:40:43 -0700

    Reformat

--- Client/src/pages/account.tsx
+++ Client/src/pages/account.tsx
@@ -78,7 +78,7 @@ export default function Account() {
                 <div className="container">
                     {account ? (
                         <Card style={{ maxWidth: '800px' }} className="mx-auto shadow">
-                            <Card.Header className='bg-primary text-white'>
+                            <Card.Header className="bg-primary text-white">
                                 <h2>{account.name}</h2>
                             </Card.Header>
                             <Card.Body>
