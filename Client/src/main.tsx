feccb291ffd31a64cb03f36da69c84d6fac86445
weisman <weisman123@outlook.com>
2024-05-16 21:52:17 -0700

    install popperjs

--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -4,6 +4,7 @@ import router from './router';
 import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
