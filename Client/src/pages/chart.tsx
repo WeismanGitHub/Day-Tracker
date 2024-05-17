6398c5e542a4a9ea8211178546f49583bde8d226
weisman <weisman123@outlook.com>
2024-05-17 03:14:48 -0700

    Set up boilerplate

--- Client/src/pages/chart.tsx
+++ Client/src/pages/chart.tsx
@@ -1,3 +1,88 @@
+import { chartSchema, problemDetailsSchema } from '../schemas';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Breadcrumb, Navbar, Toast, ToastContainer } from 'react-bootstrap';
+
 export default function Chart() {
-    return <>charts</>
+    const [chart, setChart] = useState<Chart | null>()
+    const { chartId } = useParams();
+    const navigate = useNavigate();
+
+    const [error, setError] = useState<CustomError | null>(null);
+    const [success, setSuccess] = useState<string | null>(null);
+
+    useEffect(() => {
+        (async function () {
+            try {
+                const res = await axios.get<Chart>(`/Api/Charts/${chartId}`);
+
+                if (!chartSchema.validateSync(res.data)) {
+                    throw new Error();
+                }
+
+                setChart(res.data);
+            } catch (err) {
+                if (
+                    axios.isAxiosError<CustomError>(err) &&
+                    problemDetailsSchema.isValidSync(err.response?.data)
+                ) {
+                    if (err.response.status == 401) {
+                        return navigate('/auth');
+                    }
+
+                    setError(err.response.data);
+                } else {
+                    setError({
+                        title: 'Unknown Error',
+                        detail: 'Something went wrong!',
+                        status: 500,
+                    });
+                }
+            }
+        })();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <>
+            <Navbar />
+            <div className="full-height-minus-navbar">
+                <Breadcrumb>
+                    <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
+                    <Breadcrumb.Item href={`/charts/${chartId}`}>{chart?.name}</Breadcrumb.Item>
+                </Breadcrumb>
+            </div>
+
+            <ToastContainer position="top-end">
+                <Toast
+                    onClose={() => setError(null)}
+                    show={error !== null}
+                    autohide={true}
+                    className="d-inline-block m-1"
+                    bg={'danger'}
+                >
+                    <Toast.Header>
+                        <strong className="me-auto">{error?.title}</strong>
+                    </Toast.Header>
+                    <Toast.Body>{error?.detail ?? 'Something went wrong.'}</Toast.Body>
+                </Toast>
+            </ToastContainer>
+
+            <ToastContainer position="top-end">
+                <Toast
+                    onClose={() => setSuccess(null)}
+                    show={success !== null}
+                    autohide={true}
+                    className="d-inline-block m-1"
+                    bg={'success'}
+                >
+                    <Toast.Header>
+                        <strong className="me-auto">{'Success!'}</strong>
+                    </Toast.Header>
+                    <Toast.Body>{success}</Toast.Body>
+                </Toast>
+            </ToastContainer>
+        </>
+    );
 }
