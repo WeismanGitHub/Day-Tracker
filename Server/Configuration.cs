88b118eae601c6902a543138d0b65b27fee8bf19
weisman <weisman123@outlook.com>
2024-05-28 08:18:04 -0700

    Remove jwt stuff

--- Server/Configuration.cs
+++ Server/Configuration.cs
@@ -1,13 +1,3 @@
 ﻿namespace Server;
 
-public class JwtConfig
-{
-    public required string Secret { get; set; }
-    public required string ValidIssuer { get; set; }
-    public required string ValidAudience { get; set; }
-}
-
-public class Configuration
-{
-    public required JwtConfig Jwt { get; set; }
-}
+public class Configuration { }
