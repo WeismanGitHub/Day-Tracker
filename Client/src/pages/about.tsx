4eec1ea3f07df5b6159d9508d2ca8a6385f3ec80
weisman <weisman123@outlook.com>
2024-06-07 18:40:37 -0700

    Create about page

---
+++ Client/src/pages/about.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../navbar";
+
+export default function About() {
+    return <>
+        <NavBar />
+        <div className="d-flex justify-content-center align-items-center full-height-minus-navbar">
+
+        </div>
+    </>
+}
--- Client/src/router.tsx
+++ Client/src/router.tsx
@@ -3,12 +3,14 @@ import { RequireAuthentication } from './helpers.ts';
 
 import Account from './pages/account.tsx';
 import Chart from './pages/chart.tsx';
+import About from './pages/about.tsx';
 import Home from './pages/home.tsx';
 import Auth from './pages/auth.tsx';
 
 const router = createBrowserRouter([
     { path: '/', element: <RequireAuthentication element={<Home />} /> },
     { path: '/auth', element: <Auth /> },
+    { path: '/about', element: <About /> },
     { path: '/account', element: <RequireAuthentication element={<Account />} /> },
     { path: '/charts/:chartId', element: <RequireAuthentication element={<Chart />} /> },
 ]);
