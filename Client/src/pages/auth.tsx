cdf7cafa2c47e8a42cc0246c45c0beab28afada7
weisman <weisman123@outlook.com>
2024-07-18 16:55:55 -0700

    Add a generate credentials button

--- Client/src/pages/auth.tsx
+++ Client/src/pages/auth.tsx
@@ -31,7 +31,7 @@ const signupSchema = signinSchema.shape({
 });
 
 export default function Auth() {
-    const [showSignin, setShowSignin] = useState<boolean>(true);
+    const [showSignin, setShowSignin] = useState<boolean>(false);
     const [error, setError] = useState<CustomError | null>(null);
 
     return (
@@ -138,75 +138,115 @@ function Signup({
                     }
                 }}
             >
-                {({ handleSubmit, handleChange, values, errors }) => (
-                    <Form noValidate onSubmit={handleSubmit}>
-                        <Row className="mb-3">
-                            <FormGroup as={Col} controlId="nameId">
-                                <FormLabel>Name</FormLabel>
-                                <InputGroup hasValidation>
-                                    <FormControl
-                                        autoFocus
-                                        autoComplete="on"
-                                        type="text"
-                                        aria-describedby="inputGroupPrepend"
-                                        name="name"
-                                        value={values.name}
-                                        onChange={handleChange}
-                                        isInvalid={!!errors.name}
-                                    />
-                                    <FormControl.Feedback type="invalid">{errors.name}</FormControl.Feedback>
-                                </InputGroup>
-                            </FormGroup>
-                        </Row>
-                        <Row className="mb-3">
-                            <FormGroup as={Col} controlId="PasswordID">
-                                <FormLabel>Password</FormLabel>
-                                <InputGroup hasValidation>
+                {({ handleSubmit, handleChange, values, errors, setFieldValue, setValues }) => {
+                    function generateString(limit: number): string {
+                        const characters = 'abcdefghijklmnopqrstuvwxyz';
+                        const chaactersLength = characters.length;
+                        let result = '';
+
+                        for (let i = 0; i < limit; i++) {
+                            result += characters.charAt(Math.floor(Math.random() * chaactersLength));
+                        }
+
+                        return result;
+                    }
+
+                    async function generateCredentials() {
+                        const name = generateString(7);
+                        const password =
+                            generateString(10) +
+                            generateString(1).toUpperCase() +
+                            Math.floor(Math.random() * 10);
+
+                        setFieldValue('name', name);
+                        setFieldValue('password', password);
+                        setFieldValue('confirmPassword', password);
+                        setValues({
+                            name,
+                            password,
+                            confirmPassword: password,
+                        });
+                    }
+
+                    return (
+                        <Form noValidate onSubmit={handleSubmit}>
+                            <Row className="mb-3">
+                                <FormGroup as={Col} controlId="nameId">
+                                    <FormLabel>Name</FormLabel>
+                                    <InputGroup hasValidation>
+                                        <FormControl
+                                            autoFocus
+                                            autoComplete="on"
+                                            type="text"
+                                            aria-describedby="inputGroupPrepend"
+                                            name="name"
+                                            value={values.name}
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.name}
+                                        />
+                                        <FormControl.Feedback type="invalid">
+                                            {errors.name}
+                                        </FormControl.Feedback>
+                                    </InputGroup>
+                                </FormGroup>
+                            </Row>
+                            <Row className="mb-3">
+                                <FormGroup as={Col} controlId="PasswordID">
+                                    <FormLabel>Password</FormLabel>
+                                    <InputGroup hasValidation>
+                                        <FormControl
+                                            autoComplete="on"
+                                            type="password"
+                                            aria-describedby="inputGroupPrepend"
+                                            name="password"
+                                            value={values.password}
+                                            onChange={handleChange}
+                                            isInvalid={!!errors.password}
+                                        />
+                                        <FormControl.Feedback type="invalid">
+                                            {errors.password}
+                                        </FormControl.Feedback>
+                                    </InputGroup>
+                                </FormGroup>
+                            </Row>
+                            <Row className="mb-3">
+                                <FormGroup as={Col} controlId="ConfirmPasswordID">
+                                    <FormLabel>Confirm Password</FormLabel>
                                     <FormControl
                                         autoComplete="on"
                                         type="password"
-                                        aria-describedby="inputGroupPrepend"
-                                        name="password"
-                                        value={values.password}
+                                        name="confirmPassword"
+                                        value={values.confirmPassword}
                                         onChange={handleChange}
-                                        isInvalid={!!errors.password}
+                                        isInvalid={!!errors.confirmPassword}
                                     />
                                     <FormControl.Feedback type="invalid">
-                                        {errors.password}
+                                        {errors.confirmPassword}
                                     </FormControl.Feedback>
-                                </InputGroup>
-                            </FormGroup>
-                        </Row>
-                        <Row className="mb-3">
-                            <FormGroup as={Col} controlId="ConfirmPasswordID">
-                                <FormLabel>Confirm Password</FormLabel>
-                                <FormControl
-                                    autoComplete="on"
-                                    type="password"
-                                    name="confirmPassword"
-                                    value={values.confirmPassword}
-                                    onChange={handleChange}
-                                    isInvalid={!!errors.confirmPassword}
-                                />
-                                <FormControl.Feedback type="invalid">
-                                    {errors.confirmPassword}
-                                </FormControl.Feedback>
-                            </FormGroup>
-                        </Row>
-                        <Row>
-                            <Col className="d-flex justify-content-end">
-                                <Button type="submit">Sign Up</Button>
-                                <Button
-                                    variant="secondary"
-                                    className="ms-1"
-                                    onClick={() => setShowSignin(!showSignin)}
-                                >
-                                    Sign In
-                                </Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                )}
+                                </FormGroup>
+                            </Row>
+                            <Row>
+                                <Col className="d-flex justify-content-end">
+                                    <Button type="submit">Sign Up</Button>
+                                    <Button
+                                        className="ms-1 text-white"
+                                        variant="info"
+                                        onClick={generateCredentials}
+                                    >
+                                        Generate
+                                    </Button>
+                                    <Button
+                                        variant="secondary"
+                                        className="ms-1"
+                                        onClick={() => setShowSignin(!showSignin)}
+                                    >
+                                        Sign In
+                                    </Button>
+                                </Col>
+                            </Row>
+                        </Form>
+                    );
+                }}
             </Formik>
         </>
     );
