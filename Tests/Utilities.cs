f1e7f12dd6580f87d2e6d864419ff03744b7ea12
weisman <weisman123@outlook.com>
2024-07-28 23:20:16 -0700

    Create

---
+++ Tests/Utilities.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests;
+
+public static class Constants
+{
+    public const string ValidPassword = "ValidPassword1";
+    public const string ValidUsername = "ValidUsername";
+}
+
+public static class Database
+{
+    public static void Initialize() { }
+}
