867aa46df763626ee49d18a2fdce59794b3d2e33
weisman <weisman123@outlook.com>
2024-07-29 19:27:50 -0700

    Create tests

---
+++ Tests/UsersTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Tests
+{
+    public class UsersTests(WebApplicationFactory<Program> factory)
+        : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory = factory;
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public async Task SignUp_WithInvalidName_ThrowsValidationException(string name)
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.PostAsJsonAsync(
+                "/API/Users/SignUp",
+                new { name, Password = Constants.ValidPassword }
+            );
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
